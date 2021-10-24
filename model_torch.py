0e566afca4b30fde27fd2e2f4f77f8fcab71d234
otakbeku <iceramngl@gmail.com>
2021-10-24 14:40:01 +0700

    First init

---
+++ model_torch.py
@@ -0,0 +1,82 @@
+from typing import ForwardRef
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import time
+import os
+import matplotlib.pyplot as plt
+import re
+import numpy as np
+import scipy.io as scio
+
+
+# Dataloader
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from skimage import io, transform
+
+from PIL import Image
+
+
+# ignore warning
+import warnings
+warnings.filterwarnings('ignore')
+
+
+class T_CNN(nn.Module):
+    def __init__(self,
+                 ) -> None:
+        super(T_CNN, self).__init__()
+
+        self.conv_1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv_2 = nn.Conv2d(16, 16, 3, padding=1)
+        # self.conv_3 = nn.Conv2d(16, 16, 3, padding=1)
+        # Concatenate
+        self.conv_4 = nn.Conv2d((16+16+16+3), 16, 3, padding=1)
+        # self.conv_5 = nn.Conv2d(16, 16, 3, padding=1)
+        # self.conv_6 = nn.Conv2d(16, 16, 3, padding=1)
+        # Concatenate
+        self.conv_7 = nn.Conv2d(((16+16+16+3)+16+16+16), 16, 3, padding=1)
+        # self.conv_8 = nn.Conv2d(16, 16, 3, padding=1)
+        # self.conv_9 = nn.Conv2d(16, 16, 3, padding=1)
+        # Concatenate
+        self.conv_10 = nn.Conv2d(147, 3, 3, padding=1)
+
+    def forward(self, x):
+        x1 = F.relu(self.conv_1(x))  # 1
+        x2 = F.relu(self.conv_2(x1))  # 2
+        x3 = F.relu(self.conv_2(x2))  # 3
+        x_concat1 = torch.cat((x1, x2, x3, x), dim=1)
+        x4 = F.relu(self.conv_4(x_concat1))  # 4
+        x5 = F.relu(self.conv_2(x1))  # 5
+        x6 = F.relu(self.conv_2(x2))  # 6
+        x_concat2 = torch.cat((x_concat1, x4, x5, x6), dim=1)
+        x7 = F.relu(self.conv_7(x_concat2))  # 7
+        x8 = F.relu(self.conv_2(x1))  # 8
+        x9 = F.relu(self.conv_2(x2))  # 9
+        x_concat3 = torch.cat((x_concat2, x7, x8, x9), dim=1)
+        x_out = self.conv_10(x_concat3)
+        x_out += x
+        return x_out
+
+
+class Paired_Dataset(Dataset):
+    def __init__(self, train_folder, target_folder, transform=None):
+        self.train_folder = train_folder
+        self.target_folder = target_folder
+        self.image_list = os.listdir(self.train_folder)
+        self.transform = transform
+
+    def __len__(self) -> int:
+        return len(self.image_list)
+
+    def __getitem__(self, index: int):
+        train_image = Image.open(os.path.join(
+            self.train_folder, self.image_list[index]))
+        target_image = Image.open(os.path.join(
+            self.target_folder, self.image_list[index]))
+        if self.transform:
+            train_image = self.transform(train_image)
+            target_image = self.transform(target_image)
+        return train_image, target_image
---
+++ train_torch.py
@@ -0,0 +1,149 @@
+import imgaug
+import torch
+import torch.nn as nn
+from torchvision import transforms
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+from torch.utils.data.sampler import SubsetRandomSampler
+from kornia import losses
+
+from time import time
+import random
+import numpy as np
+
+import wandb
+
+from model_torch import *
+
+# ================================== Utils
+import gc
+import os
+
+SEED = 303
+
+MODEL_CONFIG = {
+    'epochs': 2000,
+    'seed': 101,
+    'train_folder':"data/EUVP dataset/modified_pairs/trainA",
+    'target_folder':"data/EUVP dataset/modified_pairs/trainB",
+    'lr':0.001,
+    'weight_decay': 1e-05,
+    'root_path': "/",
+    'model_name':'UWCNN'
+}
+
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda:0"
+
+
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+
+
+def set_seed(seed):
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    imgaug.seed(seed)
+
+
+def data_loader(train_folder, target_folder, batch_size=1, test_size=0.2, valid_size=0.2):
+    set_seed(MODEL_CONFIG['seed'])
+    #    image_height=230,
+    #    image_width=310,
+    transformer = transforms.Compose([transforms.Resize((230, 310)),transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
+    dataset = Paired_Dataset(train_folder, target_folder, transform=transformer)
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+
+    np.random.shuffle(indices)
+    test_split = int(np.floor(test_size * dataset_size))
+    train_indices = indices[test_split:]
+    test_indices = indices[:test_split]
+
+    train_size = len(train_indices)
+    valid_split = int(np.floor((1 - valid_size) * train_size))
+    train_indices, valid_indices = train_indices[:
+                                                 valid_split], train_indices[valid_split:]
+
+    valid_sampler = SubsetRandomSampler(valid_indices)
+    valid_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=valid_sampler,
+    )
+
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    train_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=train_sampler,
+    )
+
+    test_sampler = SubsetRandomSampler(test_indices)
+    test_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=test_sampler,
+        num_workers=6
+    )
+    return train_loader, valid_loader, test_loader
+
+def mse_ssim(output, target):
+    # loss func:
+    # MSE + SSIM
+    mse_loss = nn.MSELoss()(output, target)
+    ssim_loss = losses.ssim_loss(output, target, window_size=13)
+    loss = mse_loss+ssim_loss
+    return loss
+
+model = T_CNN()
+optimizer = optim.SGD(model.parameters(), lr=MODEL_CONFIG['lr'], weight_decay=MODEL_CONFIG['weight_decay'])
+train_loader, valid_loader, test_loader = data_loader(MODEL_CONFIG['train_folder'],MODEL_CONFIG['target_folder'])
+
+# criterion = mse_ssim()
+validation_loss_min = np.inf
+# t_start = time()
+print('[INFO] Begin training')
+for epoch in range(MODEL_CONFIG.get('epochs')):
+    train_loss = 0.0
+    validation_loss = 0.0
+
+    model.train()
+    for data, target in train_loader:
+        data = data.to(device)
+        target = target.to(device)
+
+        optimizer.zero_grad()
+        output = model(data)
+        loss = mse_ssim(output, target)
+
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+
+    train_loss = train_loss / len(train_loader.sampler)
+
+    model.eval()
+    for data, target in valid_loader:
+        data = data.to(device)
+        target = target.to(device)
+
+        output = model(data)
+        loss = mse_ssim(output, target)
+        validation_loss += loss.item()
+        if validation_loss <= validation_loss_min:
+            print(f'\t\tSmaller validation loss! {validation_loss_min} --> {validation_loss}')
+            path = os.path.join(MODEL_CONFIG['root_path'], "checkpoint_best")
+            torch.save((model.state_dict(), optimizer.state_dict()), path+f"/{MODEL_CONFIG['model_name']}_best.pt")
+            validation_loss_min = validation_loss
+    gc.collect()
+
+    validation_loss = validation_loss / len(valid_loader.sampler)
+    print(f'\tEPOCH: {epoch+1}\ttrain loss: {train_loss}\tValidation loss: {validation_loss}')
+torch.save(model.state_dict(), f"{MODEL_CONFIG['model_name']}last_epoch_model.pt")
